f97f95b7d8087b8eff50ea6350ed0fd719427f3a
Issam-elka <80962666+Issam-elka@users.noreply.github.com>
2021-07-21 21:26:35 +0200

    serie2-exo6

--- Laravel-revision-serie2/resources/views/template/main.blade.php
+++ Laravel-revision-serie2/resources/views/template/main.blade.php
@@ -1,25 +1,77 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Document</title>
 </head>
+
 <body>
     <header>
         <div class="d-flex">
             <ul class="d-flex">
-                <li> <a href="{{route('view1')}}">View1</a></li>
-                <li> <a href="{{route('view2')}}">View2</a></li>
-                <li> <a href="{{route('view3')}}">View3</a></li>
+                <li> <a href="{{ route('view1') }}">View1</a></li>
+                <li> <a href="{{ route('view2') }}">View2</a></li>
+                <li> <a href="{{ route('view3') }}">View3</a></li>
             </ul>
+        </div>
+        <div class="container">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="{{asset('img/plat1.jpg')}}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{asset('img/plat2.jpg')}}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{asset('img/red.png')}}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{asset('img/orange.jpg')}}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{asset('img/animal1.jpg')}}" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="{{asset('img/animal2.jpg')}}" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
     </header>
 
     @yield('content')
 
-    <script src="{{asset('js/app.js')}}"></script>
+    <footer>
+        <div class="card text-center">
+            <div class="card-header">
+                Featured
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+            <div class="card-footer text-muted">
+                2 days ago
+            </div>
+        </div>
+    </footer>
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
